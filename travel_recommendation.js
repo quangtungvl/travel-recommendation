43eac029b33192f6c07c1614fb7a6c3b6014da3c
Quang Tung <quangtungvl@gmail.com>
2024-05-16 12:09:54 +0700

    finish

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,14 +1,15 @@
-const input = document.getElementById('input').value.toLowerCase();
+
 const btnSearch = document.getElementById('btnSearch');
 const btnClear = document.getElementById('btnClear');
-
+const cardContainerDiv = document.getElementById('card-container');
 
 function searchRecommendation() {
+    const input = document.getElementById('input').value.toLowerCase();
 
-    const cardContainerDiv = document.getElementById('card-container');
     cardContainerDiv.innerHTML = '';
 
     var recommendation;
+    var country;
     let is_found = false;
     fetch('travel_recommendation_api.json')
         .then(response => response.json())
@@ -20,31 +21,44 @@ function searchRecommendation() {
                     recommendation = data.beaches;
                     is_found = true;
                     break;
-                case 'countries':
-                case 'country':
-                case 'countrie':
-                case 'countri':
-                case 'countr':
-                    recommendation = data.countries;
-                    is_found = true;
-                    break;
                 case 'temples':
                 case 'temple':
                 case 'temple':
                     recommendation = data.temples;
                     is_found = true;
                     break;
+                case 'australia':
+                    country = data.countries[0];
+                    recommendation = country.cities;
+                    is_found = true;
+                    break;
+                case 'japan':
+                    country = data.countries[1];
+                    recommendation = country.cities;
+                    is_found = true;
+                    break;
+                case 'brazil':
+                    country = data.countries[2];
+                    recommendation = country.cities;
+                    is_found = true;
+                    break;
+
             }
-            if(is_found){
+            if (is_found) {
                 recommendation.forEach(item => {
-                    const cardDiv = document.createElement('card');
-                    cardDiv.innerHTML+=`<img src="assets/img/${item.imageUrl}" alt="${item.description}" />`;
-                    cardDiv.innerHTM += `<div class="card__content">`;
-                    cardDiv.innerHTM += `<h2>${item.name}</h2>`;
-                    cardDiv.innerHTM += `<p>${item.description}</p>`;
-                    cardDiv.innerHTM += `<button class="btn">Explore</button>`;
-                    cardDiv.innerHTM += `</div>`;
-                    cardContainerDiv.addchild(cardDiv);
+                    const cardDiv = document.createElement('div');
+                    cardDiv.className = 'card';
+                    cardDiv.innerHTML += `<img src="assets/img/${item.imageUrl}" alt="${item.description}" />`;
+
+                    const cardContentDiv = document.createElement('div');
+                    cardContentDiv.className = 'card__content';
+
+                    cardContentDiv.innerHTML += `<h2>${item.name}</h2>`;
+                    cardContentDiv.innerHTML += `<p>${item.description}</p>`;
+                    cardContentDiv.innerHTML += `<button class="btn">Visit</button>`;
+
+                    cardDiv.appendChild(cardContentDiv);
+                    cardContainerDiv.appendChild(cardDiv);
                 });
             }
         })
@@ -56,7 +70,7 @@ function searchRecommendation() {
 }
 
 function clearRecommendation() {
-
+    cardContainerDiv.innerHTML = '';
 }
 
 
